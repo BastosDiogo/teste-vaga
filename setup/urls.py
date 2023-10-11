59a2d2c51b7fd5ffadba4a73a793022913172e3c
BastosDiogo <bmnetto.diogo@gmail.com>
2023-10-11 09:02:52 -0300

    Add estruturas

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from api.views import (
+    ProdutosViewSets,
+    CategoriaViewSets,
+    FornecedorViewSets
+)
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('api-produto', ProdutosViewSets, 'api_produto')
+router.register('api-categoria', CategoriaViewSets, 'api_categoria')
+router.register('api-fornecedor', FornecedorViewSets, 'api_fornecedor')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("produtos.urls"))
+    path('', include("produtos.urls")),
+    path('api/', include(router.urls)),
 ]
