57252ab4785257832e573a18eb53e24b640c13af
BastosDiogo <bmnetto.diogo@gmail.com>
2023-10-11 09:02:19 -0300

    Add modal e endpoints.

---
+++ api/serializer.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from api.models import Produtos, Categoria, Fornecedor
+
+class ProdutoSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Produtos
+        fields = [
+            'nome',
+            'categoria',
+            'data_cadastro',
+            'data_atualizacao',
+            'fornecedores',
+            'descricao'
+        ]
+
+
+class CategoriaSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
+
+class FornecedorSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Fornecedor
+        fields = '__all__'
